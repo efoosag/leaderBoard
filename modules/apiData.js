66107cca2923433ed31c51c38308cf6219ac280f
efoosag <96612103+efoosag@users.noreply.github.com>
2022-06-22 15:31:50 +0100

    add apiData.js

---
+++ modules/apiData.js
@@ -0,0 +1,22 @@
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/YDfzW6xi7ve1PiNDrUVj/scores';
+
+const retrieveData = async () => {
+  const response = await fetch(url);
+  const scores = await response.json();
+  return scores;
+};
+
+// const setScores = async (player, scores) => {
+//   const information = await fetch(url, {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ user: player, score: scores }),
+//   });
+//   const allScore = information.json();
+//   return allScore;
+// };
+
+export { retrieveData, setScores };
